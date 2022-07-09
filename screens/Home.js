e89fea425be22280c0e93fa99e8e423ed152c74f
matsuda-argano <matsuda@argano.co.jp>
2022-07-09 22:51:21 +0900

    layout

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,31 @@
-import { Text } from "react-native"
+import { View, SafeAreaView, FlatList, Text } from 'react-native';
+
+import { COLORS, NFTData } from '../constants';
+import { NFTCard, HomeHeader, FocusedStatusBar } from '../components';
 
 const Home = () => {
   return (
-    <Text>Home</Text>
-  )
-}
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
 
-export default Home
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+
+        <View style={{ position: 'absolute', top: 0, botto: 0, right: 0, left: 0, zIndex: -1 }}>
+          <View style={{ height: 300, background: COLORS.primary }} />
+          <View style={{ flex: 1, background: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
